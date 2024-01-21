23f6342ed157c75b256c8b1eff8518b9ac2a51bf
Salim Benfarhat <hello@salim.link>
2024-01-21 10:34:03 +0100

    21/01/2024 - Adding comments on gallery file

--- scripts/utils/gallery.js
+++ scripts/utils/gallery.js
@@ -1,12 +1,20 @@
+// Fonction pour mettre à jour le nombre total de likes
 function updateTotalLikes(totalLikes) {
+  // Sélection de l'élément du DOM pour afficher le total des likes
   const likesSpan = document.getElementById("totalLikes");
+  // Mise à jour du contenu textuel de l'élément avec le total des likes
   likesSpan.textContent = `${totalLikes} `;
+  // Création d'une icône de coeur
   const heartIcon = document.createElement("i");
   heartIcon.className = "fas fa-heart";
+  // Ajout de l'icône de coeur à l'élément
   likesSpan.appendChild(heartIcon);
 }
 
+// Fonction pour mettre à jour le prix
 function updatePrice(price) {
+  // Sélection de l'élément du DOM pour afficher le prix
   const priceSpan = document.getElementById("price");
+  // Mise à jour du contenu textuel de l'élément avec le prix
   priceSpan.textContent = `${price}€ / jour`;
 }
