719d126e87ff25ec5e4061f8ecf84c6154f1ed15
Salim Benfarhat <hello@salim.link>
2024-01-21 10:13:01 +0100

    21/01/2024 - Creating file for keyboard listeners

---
+++ scripts/utils/keyboardListeners.js
@@ -0,0 +1,22 @@
+// Fonction pour ajouter un écouteur d'événements qui réagit à la touche 'Échap'
+function addEscapeListener(callback) {
+    // Ajout d'un écouteur d'événements 'keydown' au document
+    document.addEventListener("keydown", function (event) {
+      // Si la touche pressée est 'Échap', exécute la fonction de rappel
+      if (event.code === "Escape") {
+        callback();
+      }
+    });
+}
+
+// Fonction pour ajouter un écouteur d'événements qui réagit à la touche 'Entrée'
+function addEnterListener(element, callback) {
+    // Ajout d'un écouteur d'événements 'keydown' à l'élément spécifié
+    element.addEventListener("keydown", function (event) {
+      // Si la touche pressée est 'Entrée', exécute la fonction de rappel et annule l'événement
+      if (event.code === "Enter") {
+        callback();
+        event.preventDefault();
+      }
+    });
+}
